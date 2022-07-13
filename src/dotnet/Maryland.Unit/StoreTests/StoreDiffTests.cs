86dfa73e67d2d508f9f48383c52c4431f143eb24
jameswilddev <jameswilddev@outlook.com>
2022-07-13 20:55:17 +0100

    Add store diffs.

---
+++ src/dotnet/Maryland.Unit/StoreTests/StoreDiffTests.cs
@@ -0,0 +1,73 @@
+﻿using Maryland.Stores;
+using Maryland.Stores.ReferenceStores;
+using Maryland.Stores.TagStores;
+using System.Collections.Immutable;
+
+namespace Maryland.Unit.StoreTests
+{
+    [TestClass]
+    public sealed class StoreDiffTests
+    {
+        [TestMethod]
+        public void ExposesReferenceStore()
+        {
+            var referenceStoreDiff = new ReferenceStoreDiff(ImmutableDictionary<EntityAttributeIdentifierPair, Guid>.Empty, ImmutableHashSet<EntityAttributeIdentifierPair>.Empty);
+            var tagStoreDiff = new TagStoreDiff(ImmutableDictionary<Guid, string>.Empty, ImmutableHashSet<Guid>.Empty);
+
+            var storeDiff = new StoreDiff(referenceStoreDiff, tagStoreDiff);
+
+            Assert.AreSame(referenceStoreDiff, storeDiff.ReferenceStoreDiff);
+        }
+
+        [TestMethod]
+        public void ExposesTagStore()
+        {
+            var referenceStoreDiff = new ReferenceStoreDiff(ImmutableDictionary<EntityAttributeIdentifierPair, Guid>.Empty, ImmutableHashSet<EntityAttributeIdentifierPair>.Empty);
+            var tagStoreDiff = new TagStoreDiff(ImmutableDictionary<Guid, string>.Empty, ImmutableHashSet<Guid>.Empty);
+
+            var storeDiff = new StoreDiff(referenceStoreDiff, tagStoreDiff);
+
+            Assert.AreSame(tagStoreDiff, storeDiff.TagStoreDiff);
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWhenReferenceStoreNull()
+        {
+            var tagStoreDiff = new TagStoreDiff(ImmutableDictionary<Guid, string>.Empty, ImmutableHashSet<Guid>.Empty);
+
+            try
+            {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                _ = new StoreDiff(null, tagStoreDiff);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                Assert.Fail();
+            }
+            catch (ArgumentNullException exception)
+            {
+                Assert.AreEqual("referenceStoreDiff", exception.ParamName);
+                Assert.AreEqual("Value cannot be null. (Parameter 'referenceStoreDiff')", exception.Message);
+                Assert.IsNull(exception.InnerException);
+            }
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWhenTagStoreNull()
+        {
+            var referenceStoreDiff = new ReferenceStoreDiff(ImmutableDictionary<EntityAttributeIdentifierPair, Guid>.Empty, ImmutableHashSet<EntityAttributeIdentifierPair>.Empty);
+
+            try
+            {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                _ = new StoreDiff(referenceStoreDiff, null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                Assert.Fail();
+            }
+            catch (ArgumentNullException exception)
+            {
+                Assert.AreEqual("tagStoreDiff", exception.ParamName);
+                Assert.AreEqual("Value cannot be null. (Parameter 'tagStoreDiff')", exception.Message);
+                Assert.IsNull(exception.InnerException);
+            }
+        }
+    }
+}
---
+++ src/dotnet/Maryland/Stores/StoreDiff.cs
@@ -0,0 +1,43 @@
+﻿using Maryland.Stores.ReferenceStores;
+using Maryland.Stores.TagStores;
+
+namespace Maryland.Stores
+{
+    /// <summary>
+    /// A description of the differences between two <see cref="Store"/>s.
+    /// </summary>
+    public sealed class StoreDiff
+    {
+        /// <summary>
+        /// The differences between the <see cref="ReferenceStore"/>s within the <see cref="Store"/>s.
+        /// </summary>
+        public readonly ReferenceStoreDiff ReferenceStoreDiff;
+
+        /// <summary>
+        /// The differences between the <see cref="TagStore"/>s within the <see cref="Store"/>s.
+        /// </summary>
+        public readonly TagStoreDiff TagStoreDiff;
+
+        /// <summary>
+        /// Creates a new description of the differences between two <see cref="Store"/>s.
+        /// </summary>
+        /// <param name="referenceStoreDiff">The differences between the <see cref="ReferenceStore"/>s within the <see cref="Store"/>s.</param>
+        /// <param name="tagStoreDiff">The differences between the <see cref="TagStore"/>s within the <see cref="Store"/>s.</param>
+        public StoreDiff(ReferenceStoreDiff referenceStoreDiff, TagStoreDiff tagStoreDiff)
+        {
+            if (referenceStoreDiff == null)
+            {
+                throw new ArgumentNullException(nameof(referenceStoreDiff));
+            }
+            else if (tagStoreDiff == null)
+            {
+                throw new ArgumentNullException(nameof(tagStoreDiff));
+            }
+            else
+            {
+                ReferenceStoreDiff = referenceStoreDiff;
+                TagStoreDiff = tagStoreDiff;
+            }
+        }
+    }
+}
