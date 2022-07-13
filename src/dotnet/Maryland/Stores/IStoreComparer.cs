e3891f5972d585dd8240e5c32c02fe3e4dee8d19
jameswilddev <jameswilddev@outlook.com>
2022-07-13 21:39:10 +0100

    Add store comparer.

---
+++ src/dotnet/Maryland/Stores/IStoreComparer.cs
@@ -0,0 +1,18 @@
+﻿namespace Maryland.Stores
+{
+    /// <summary>
+    /// Generates <see cref="StoreDiff"/>s from pairs of <see cref="Store"/>s.
+    /// </summary>
+    public interface IStoreComparer
+    {
+        /// <summary>
+        /// Generates a <see cref="StoreDiff"/> which describes the changes between two <see cref="Store"/>s.
+        /// </summary>
+        /// <param name="a">The first <see cref="Store"/> to compare.</param>
+        /// <param name="b">The second <see cref="Store"/> to compare.</param>
+        /// <returns>A <see cref="StoreDiff"/> which describes the changes which need to be applied to <paramref name="a"/> to make it equivalent to <paramref name="b"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="a"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="b"/> is null.</exception>
+        StoreDiff Compare(Store a, Store b);
+    }
+}
